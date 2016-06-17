b9f884155c16ed0570cff612cf37f651bd8f716a
Soham Dodia <sohamdodia@gmail.com>
2016-06-17 12:55:37 +0530

    Refactor API with underscore

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var _ = require('underscore');
 var app = express();
 var PORT = process.env.PORT || 3000;
 
@@ -19,13 +20,13 @@ app.get('/todos',function(req,res) {
 
 app.get('/todos/:id',function(req,res) {
 	var todoId = parseInt(req.params.id,10);
-	var matchedTodo;
+	var matchedTodo = _.findWhere(todos,{id : todoId});
 
-	todos.forEach(function (todo){
-		if(todoId === todo.id) {
-			matchedTodo = todo;
-		}
-	});
+	// todos.forEach(function (todo){
+	// 	if(todoId === todo.id) {
+	// 		matchedTodo = todo;
+	// 	}
+	// });
 
 	if(matchedTodo) {
 		res.json(matchedTodo);
@@ -38,8 +39,13 @@ app.get('/todos/:id',function(req,res) {
 //POST /todos
 
 app.post('/todos',function(req,res) {
-	var body = req.body;
+
+	var body = _.pick(req.body,'description','completed');
+	if(!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length ===0) {
+		return res.status(400).send();
+	}
 	//add id field
+	body.description = body.description.trim();
 	body.id = todoNextId++;
 	//push body onto array
 	todos.push(body);
